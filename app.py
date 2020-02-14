4f547d045dbdd3c244081295d029d41971c1c5b1
Kat Jansen <17cosmiclane@gmail.com>
2020-02-13 23:10:16 -0600

    Add additional route for use of start/end routes. Reformat with black.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Resources/hawaii.sqlite'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Resources/hawaii.sqlite"
 db = SQLAlchemy(app)
+
 
 class DictMixIn:
     def to_dict(self):
@@ -15,6 +17,7 @@ class DictMixIn:
             for column in self.__table__.columns
         }
 
+
 class Measurement(db.Model, DictMixIn):
     __tablename__ = "measurement"
 
@@ -22,6 +25,7 @@ class Measurement(db.Model, DictMixIn):
     station = db.Column(db.String())
     prcp = db.Column(db.Integer())
     tobs = db.Column(db.Integer())
+
 
 class Station(db.Model, DictMixIn):
     __tablename__ = "station"
@@ -32,52 +36,86 @@ class Station(db.Model, DictMixIn):
     longitude = db.Column(db.Integer())
     elevation = db.Column(db.Integer())
 
-meas_cols = ['date','statino','prcp','tobs']
-station_cols = ['station','name','latitude','longitude','elevation']
+
+meas_cols = ["date", "statino", "prcp", "tobs"]
+station_cols = ["station", "name", "latitude", "longitude", "elevation"]
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    #list of links to other routes
+    # list of links to other routes
     return render_template("index.html")
 
-@app.route('/api/v1.0/precipitation')
+
+@app.route("/api/v1.0/precipitation")
 def prcp():
-    #Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
-    #Return the JSON representation of your dictionary.
-    prcp = db.session.query(Measurement.date,Measurement.prcp).all()
+    # Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+    # Return the JSON representation of your dictionary.
+    prcp = db.session.query(Measurement.date, Measurement.prcp).all()
     return jsonify([list(p) for p in prcp])
 
 
-@app.route('/api/v1.0/stations')
+@app.route("/api/v1.0/stations")
 def station():
-    #Return a JSON list of stations from the dataset.
-    stat = db.session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
-    return jsonify([dict(zip(station_cols,list(s))) for s in stat])
+    # Return a JSON list of stations from the dataset.
+    stat = db.session.query(
+        Station.station,
+        Station.name,
+        Station.latitude,
+        Station.longitude,
+        Station.elevation,
+    ).all()
+    return jsonify([dict(zip(station_cols, list(s))) for s in stat])
 
 
-@app.route('/api/v1.0/tobs')
+@app.route("/api/v1.0/tobs")
 def tobs():
-    #query for the dates and temperature observations from a year from the last data point.
-    #Return a JSON list of Temperature Observations (tobs) for the previous year.
-    tobs = db.session.query(Measurement.date,Measurement.tobs).all()
+    # query for the dates and temperature observations from a year from the last data point.
+    # Return a JSON list of Temperature Observations (tobs) for the previous year.
+    tobs = db.session.query(Measurement.date, Measurement.tobs).all()
     return jsonify([list(tob) for tob in tobs])
 
 
-@app.route('/api/v1.0/<start>')
-def start():
-    #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-    #When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-    #When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-    return
+@app.route("/api/v1.0/")
+def api():
+    return (
+        "Include start date with optional end date to be included after another slash"
+    )
 
 
-@app.route('/api/v1.0/<start>/<end>')
-def end():
-    #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-    #When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-    #When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-    return
+cols = ["TMIN", "TMAX", "TAVG"]
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+    # When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+    data = (
+        db.session.query(
+            func.min(Measurement.tobs),
+            func.max(Measurement.tobs),
+            func.avg(Measurement.tobs),
+        )
+        .filter(Measurement.date >= start)
+        .all()
+    )
+    return jsonify([dict(zip(cols, list(row))) for row in data])
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def end(start, end):
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+    # When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+    data = (
+        db.session.query(
+            func.min(Measurement.tobs),
+            func.max(Measurement.tobs),
+            func.avg(Measurement.tobs),
+        )
+        .filter(Measurement.date.between(start, end))
+        .all()
+    )
+    return jsonify([dict(zip(cols, list(row))) for row in data])
 
 
 if __name__ == "__main__":
